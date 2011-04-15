f15ca20571002e9e75fadfd914bf0f86f40454a4
Jens Gassmann <jens.gassmann@atomix.de>
2011-04-16 00:09:18 +0200

    Some Styles

---
+++ static/js/vCursor.js
@@ -0,0 +1,94 @@
+var vCursor = new Class({
+
+    initialize: function(calls) {
+
+        var these = this;
+        
+        this.cursor = new Element('span', {
+            class: 'vcursor'
+        });
+        
+        this.cursor.set('move', {
+            duration: 3000,
+            link: 'ignore',
+            onComplete: function() {   these.next();  }
+        });
+    
+        this.scroller = new Fx.Scroll(window, { duration: 2800 });
+        
+        
+        this.queue = new Chain();
+       
+        this.cursor.inject(document.body);
+            
+    },
+
+    // Begin Action
+    begin: function() {
+         
+      this.queue.callChain();  
+    },
+    
+    next: function(delayer) {
+        var these = this;
+        (function(){these.queue.callChain()}).delay(delayer);  
+    },
+    
+    // Move to position;
+    moveTo: function(element, speed) {
+
+        var these = this;
+        this.queue.chain(function() { these.scroller.toElement(element); these.cursor.move({ relativeTo: element }); });
+    },
+
+    // Highlight Element
+    highLight: function(element) {
+      
+        var these = this;
+        this.queue.chain(function() { element.highlight(); these.next() }); 
+    },
+    
+    // Click an Element - follow the URL if
+    clickElement: function(element) {
+
+        // Queue Move
+        this.moveTo(element);
+        
+        // Queu 
+        this.highLight( element );
+        
+        this.queue.chain(function() {
+        
+        if ( element.get('href') ) {
+         
+            (function() { window.location.href = element.get('href') }).delay(500);
+            
+        } else {
+            
+            element.fireEvent('click');            
+        }
+        
+        });
+
+    },
+
+    focusElement: function(element) {
+
+        var these = this;
+        this.moveTo(element);
+        this.highLight(element);
+        this.queue.chain(function() { element.focus(); these.next() });
+    },
+
+    typewriteText: function(element, stext, duration) {
+
+        
+        var these = this;
+        this.queue.chain( function() { element.set('value',''); these.next() });  
+        
+        stext.split("").each( function(chara) {
+            console.log(chara);
+            these.queue.chain( function() { element.set('value', element.get('value') + chara  ); these.next(duration)});
+        });
+    }
+ })
